a094e3ad1b7b266c18f89413c5fe237950c1a2df
Igor Reis <igor.reisleandro@gmail.com>
2021-05-18 21:38:30 -0300

    afa

--- src/core/controller.ts
+++ src/core/controller.ts
@@ -7,7 +7,11 @@ export class StandardController {
     protected error(response: ServerResponse, error: any): void {
         if (error instanceof FriendlyError && error.comiesDomain){
             switch (error.code){
+                case'INVALID': response.statusCode = 400; response.write(JSON.stringify(response)); break;
                 case'MISSING': response.statusCode = 400; response.write(JSON.stringify(response)); break;
+                case'UNAUTHORIZED': response.statusCode = 401; response.write(JSON.stringify(response)); break;
+                case'FORBIDDEN': response.statusCode = 403; response.write(JSON.stringify(response)); break;
+                case'NOTFOUND': response.statusCode = 404; response.write(JSON.stringify(response)); break;
                 default: break;
             }
         }
